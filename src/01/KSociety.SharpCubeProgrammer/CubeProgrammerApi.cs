61e72d076fc1a5c37c6cf8246eecbcf2b236edda
maniglia <kSociety85@gmail.com>
2024-01-22 09:28:11 +0100

    Testing

--- src/01/KSociety.SharpCubeProgrammer/CubeProgrammerApi.cs
+++ src/01/KSociety.SharpCubeProgrammer/CubeProgrammerApi.cs
@@ -104,7 +104,7 @@ namespace KSociety.SharpCubeProgrammer
                     {
                         var currentItem = Marshal.PtrToStructure<DebugConnectParameters>(listPtr + (i * size));
                         parametersList.Add(currentItem);
-                        Marshal.DestroyStructure<DebugConnectParameters>(listPtr + (i * size));
+                        //Marshal.DestroyStructure<DebugConnectParameters>(listPtr + (i * size));
                     }
                 }
                 else
@@ -136,7 +136,7 @@ namespace KSociety.SharpCubeProgrammer
                     {
                         var currentItem = Marshal.PtrToStructure<DebugConnectParameters>(listPtr + (i * size));
                         parametersList.Add(currentItem);
-                        Marshal.DestroyStructure<DebugConnectParameters>(listPtr + (i * size));
+                        //Marshal.DestroyStructure<DebugConnectParameters>(listPtr + (i * size));
                     }
                 }
                 else
