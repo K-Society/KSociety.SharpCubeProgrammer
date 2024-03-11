f068f5c78f8d6642b27d0ae929d50d39b64ce06c
maniglia <kSociety85@gmail.com>
2024-03-11 15:17:57 +0100

    Update Dispose strategy.

--- src/01/KSociety.SharpCubeProgrammer/CubeProgrammerApi.cs
+++ src/01/KSociety.SharpCubeProgrammer/CubeProgrammerApi.cs
@@ -4,18 +4,20 @@ namespace SharpCubeProgrammer
 {
     using System;
     using System.Collections.Generic;
+    using System.Diagnostics.CodeAnalysis;
     using System.Globalization;
     using System.IO;
     using System.Runtime.InteropServices;
+    using System.Threading;
+    using System.Threading.Tasks;
     using DeviceDataStructure;
     using Enum;
     using Interface;
     using Microsoft.Extensions.Logging;
     using Microsoft.Extensions.Logging.Abstractions;
     using Struct;
-    using Util;
 
-    public class CubeProgrammerApi : Disposable, ICubeProgrammerApi
+    public class CubeProgrammerApi : ICubeProgrammerApi
     {
         /// <summary>
         /// Synchronization object to protect loading the native library and its functions. This field is read-only.
@@ -24,6 +26,9 @@ namespace SharpCubeProgrammer
 
         private Native.SafeLibraryHandle _handle;
         private readonly ILogger<CubeProgrammerApi> _logger;
+
+        private const int DisposedFlag = 1;
+        private int _isDisposed;
 
         #region [Constructor]
 
@@ -1517,8 +1522,27 @@ namespace SharpCubeProgrammer
 
         #region [Dispose]
 
-        /// <inheritdoc/>
-        protected override void Dispose(bool disposing)
+        /// <summary>
+        /// Performs application-defined tasks associated with freeing, releasing, or resetting unmanaged resources.
+        /// </summary>
+        [SuppressMessage("Microsoft.Design", "CA1063:ImplementIDisposableCorrectly", Justification = "Dispose is implemented correctly, FxCop just doesn't see it.")]
+        public void Dispose()
+        {
+            var wasDisposed = Interlocked.Exchange(ref this._isDisposed, DisposedFlag);
+            if (wasDisposed == DisposedFlag)
+            {
+                return;
+            }
+
+            this.Dispose(true);
+            GC.SuppressFinalize(this);
+        }
+
+        /// <summary>
+        /// Releases unmanaged and - optionally - managed resources.
+        /// </summary>
+        /// <param name="disposing"><c>true</c> to release both managed and unmanaged resources; <c>false</c> to release only unmanaged resources.</param>
+        protected void Dispose(bool disposing)
         {
             if (disposing)
             {
@@ -1528,8 +1552,51 @@ namespace SharpCubeProgrammer
             // Free any unmanaged objects here.
             this._handle?.Dispose();
             this._handle = null;
+        }
 
-            base.Dispose(disposing);
+        /// <summary>
+        /// Gets a value indicating whether the current instance has been disposed.
+        /// </summary>
+        protected bool IsDisposed
+        {
+            get
+            {
+                Interlocked.MemoryBarrier();
+                return this._isDisposed == DisposedFlag;
+            }
+        }
+
+        /// <inheritdoc/>
+        [SuppressMessage(
+            "Usage",
+            "CA1816:Dispose methods should call SuppressFinalize",
+            Justification = "DisposeAsync should also call SuppressFinalize (see various .NET internal implementations).")]
+        public ValueTask DisposeAsync()
+        {
+            // Still need to check if we've already disposed; can't do both.
+            var wasDisposed = Interlocked.Exchange(ref this._isDisposed, DisposedFlag);
+            if (wasDisposed != DisposedFlag)
+            {
+                GC.SuppressFinalize(this);
+
+                // Always true, but means we get the similar syntax as Dispose,
+                // and separates the two overloads.
+                return this.DisposeAsync(true);
+            }
+
+            return default;
+        }
+
+        /// <summary>
+        ///  Releases unmanaged and - optionally - managed resources, asynchronously.
+        /// </summary>
+        /// <param name="disposing"><c>true</c> to release both managed and unmanaged resources; <c>false</c> to release only unmanaged resources.</param>
+        protected ValueTask DisposeAsync(bool disposing)
+        {
+            // Default implementation does a synchronous dispose.
+            this.Dispose(disposing);
+
+            return default;
         }
 
         #endregion
