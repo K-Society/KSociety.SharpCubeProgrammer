2e80dfe697b09e88338e4fa7dddb05001853847f
maniglia <kSociety85@gmail.com>
2024-05-13 15:24:12 +0200

    Fix loaderPath in dllMain.cpp

--- src/01/Programmer/dllmain.cpp
+++ src/01/Programmer/dllmain.cpp
@@ -1,30 +1,55 @@
 // dllmain.cpp : Defines the entry point for the DLL application.
 #include "pch.h"
 
-const char* loaderPath = "./st/Programmer";
+const wchar_t* loaderPath = L"..\\..\\st\\Programmer";
 
 BOOL APIENTRY DllMain( HMODULE hModule,
                        DWORD  ul_reason_for_call,
                        LPVOID lpReserved
                        )
 {
-    switch (ul_reason_for_call)
+    UNREFERENCED_PARAMETER(lpReserved);
+
+    WCHAR dllName[MAX_PATH + 1];
+    DWORD size = GetModuleFileNameW(hModule, dllName, MAX_PATH);
+
+    if (size > 0)
     {
-        case DLL_PROCESS_ATTACH:
-            /* Set device loaders path that contains FlashLoader and ExternalLoader folders*/
-            SetLoadersPath(loaderPath);
-            break;
+        std::wstring dllNameStr(dllName);
+        std::size_t pos = dllNameStr.size() - 14;
+        dllNameStr = dllNameStr.substr(0, pos);
+        dllNameStr += loaderPath;
+        std::replace(dllNameStr.begin(), dllNameStr.end(), '\\', '/');
+        const wchar_t* input = dllNameStr.c_str();
 
-        case DLL_THREAD_ATTACH:
-            break;
+        // Count required buffer size (plus one for null-terminator).
+        size_t bufferSize = (wcslen(input) + 1) * sizeof(wchar_t);
+        char* buffer = new char[bufferSize];
 
-        case DLL_THREAD_DETACH:
-            break;
+        size_t convertedSize;
+        wcstombs_s(&convertedSize, buffer, bufferSize, input, bufferSize);
+        
+        switch (ul_reason_for_call)
+        {
+            case DLL_PROCESS_ATTACH:
+                /* Set device loaders path that contains FlashLoader and ExternalLoader folders*/
+                SetLoadersPath(buffer);
+                break;
 
-        case DLL_PROCESS_DETACH:
-            break;
+            case DLL_THREAD_ATTACH:
+                break;
+
+            case DLL_THREAD_DETACH:
+                break;
+
+            case DLL_PROCESS_DETACH:
+                break;
+        }
+
+        delete[] buffer;
+        return TRUE;
     }
 
-    return TRUE;
+    return FALSE;
 }
 
