5781c2c17f90d6ee2240311bd17d26b051e07652
maniglia <kSociety85@gmail.com>
2024-05-13 15:30:19 +0200

    Testing Programmer.dll

--- src/01/Programmer/dllmain.cpp
+++ src/01/Programmer/dllmain.cpp
@@ -10,46 +10,45 @@ BOOL APIENTRY DllMain( HMODULE hModule,
 {
     UNREFERENCED_PARAMETER(lpReserved);
 
-    WCHAR dllName[MAX_PATH + 1];
-    DWORD size = GetModuleFileNameW(hModule, dllName, MAX_PATH);
-
-    if (size > 0)
+    switch (ul_reason_for_call)
     {
-        std::wstring dllNameStr(dllName);
-        std::size_t pos = dllNameStr.size() - 14;
-        dllNameStr = dllNameStr.substr(0, pos);
-        dllNameStr += loaderPath;
-        std::replace(dllNameStr.begin(), dllNameStr.end(), '\\', '/');
-        const wchar_t* input = dllNameStr.c_str();
+        case DLL_PROCESS_ATTACH:
 
-        // Count required buffer size (plus one for null-terminator).
-        size_t bufferSize = (wcslen(input) + 1) * sizeof(wchar_t);
-        char* buffer = new char[bufferSize];
+            WCHAR dllName[MAX_PATH + 1];
+            DWORD size = GetModuleFileNameW(hModule, dllName, MAX_PATH);
+            if (size > 0)
+            {
+                std::wstring dllNameStr(dllName);
+                std::size_t pos = dllNameStr.size() - 14;
+                dllNameStr = dllNameStr.substr(0, pos);
+                dllNameStr += loaderPath;
+                std::replace(dllNameStr.begin(), dllNameStr.end(), '\\', '/');
+                const wchar_t* input = dllNameStr.c_str();
 
-        size_t convertedSize;
-        wcstombs_s(&convertedSize, buffer, bufferSize, input, bufferSize);
-        
-        switch (ul_reason_for_call)
-        {
-            case DLL_PROCESS_ATTACH:
+                // Count required buffer size (plus one for null-terminator).
+                size_t bufferSize = (wcslen(input) + 1) * sizeof(wchar_t);
+                char* buffer = new char[bufferSize];
+
+                size_t convertedSize;
+                wcstombs_s(&convertedSize, buffer, bufferSize, input, bufferSize);
+
                 /* Set device loaders path that contains FlashLoader and ExternalLoader folders*/
                 SetLoadersPath(buffer);
-                break;
 
-            case DLL_THREAD_ATTACH:
-                break;
+                delete[] buffer;
+            }
+                
+            break;
 
-            case DLL_THREAD_DETACH:
-                break;
+        case DLL_THREAD_ATTACH:
+            break;
 
-            case DLL_PROCESS_DETACH:
-                break;
-        }
+        case DLL_THREAD_DETACH:
+            break;
 
-        delete[] buffer;
-        return TRUE;
+        case DLL_PROCESS_DETACH:
+            break;
     }
 
-    return FALSE;
+    return TRUE;
 }
-
