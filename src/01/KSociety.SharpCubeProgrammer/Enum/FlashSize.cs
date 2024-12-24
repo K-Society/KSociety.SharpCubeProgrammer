34abf3591fc75d74917b5b805a9353be5cccc4b8
maniglia <kSociety85@gmail.com>
2024-12-24 08:36:52 +0100

    Code style fix.

--- src/01/KSociety.SharpCubeProgrammer/Enum/FlashSize.cs
+++ src/01/KSociety.SharpCubeProgrammer/Enum/FlashSize.cs
@@ -4,8 +4,8 @@ namespace SharpCubeProgrammer.Enum
 {
     public enum FlashSize
     {
-        FlashSize1Kb = (1024),
-        FlashSize512Kb = (512 * 1024),
-        FlashSize256Kb = (256 * 1024),
+        FlashSize1Kb = 1024,
+        FlashSize512Kb = 512 * 1024,
+        FlashSize256Kb = 256 * 1024,
     }
 }
