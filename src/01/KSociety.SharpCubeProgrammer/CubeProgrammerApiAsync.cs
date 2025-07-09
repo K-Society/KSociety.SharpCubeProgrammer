ff80b53de9d03294969772d9f2cd28af94fc41fe
maniglia <kSociety85@gmail.com>
2025-07-09 16:18:31 +0200

    Fix

--- src/01/KSociety.SharpCubeProgrammer/CubeProgrammerApiAsync.cs
+++ src/01/KSociety.SharpCubeProgrammer/CubeProgrammerApiAsync.cs
@@ -5,7 +5,6 @@ namespace SharpCubeProgrammer
     using System;
     using System.Collections.Generic;
     using System.Diagnostics.CodeAnalysis;
-    using System.IO.Compression;
     using System.Threading;
     using System.Threading.Tasks;
     using SharpCubeProgrammer.DeviceDataStructure;
