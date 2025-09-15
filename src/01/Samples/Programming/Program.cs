8129b05e1697df146c1d292664b32bd6c9b3164a
maniglia <kSociety85@gmail.com>
2025-09-15 16:37:45 +0200

    Backup.

--- src/01/Samples/Programming/Program.cs
+++ src/01/Samples/Programming/Program.cs
@@ -39,7 +39,7 @@ namespace Programming
             Console.WriteLine("Press a button to continue.");
             Console.ReadLine();
 
-            var result12 = CubeProgrammerApi.VersionAPI();
+            //var result12 = CubeProgrammerApi.VersionAPI();
 
             //#region [Log Testing]
 
